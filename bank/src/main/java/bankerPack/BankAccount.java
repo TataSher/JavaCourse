fc1bececff1faa15efe7c824d19d41d5413ba037
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-24 17:33:53 +0100

    Created BankAccount class with Balance

--- bank/src/main/java/bankerPack/BankAccount.java
+++ bank/src/main/java/bankerPack/BankAccount.java
@@ -2,13 +2,18 @@ package bankerPack;
 
 public class BankAccount {
     private String name;
+    private Integer balance;
 
     public BankAccount (String name) {
         this.name = name;
+        this.balance = 0;
     }
 
     public String showName () {
         return name;
+    }
+    public Integer showBalance () {
+        return balance;
     }
 
 }
--- bank/src/test/java/bankerPack/BankAccountTest.java
+++ bank/src/test/java/bankerPack/BankAccountTest.java
@@ -5,11 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BankAccountTest {
+
     @Test
-    void testCreateBankAccount() {
+    void testCreateBankAccountWithName() {
         BankAccount account1 = new BankAccount("Kumbajan Aubergino");
         String result = account1.showName();
         assertEquals("Kumbajan Aubergino", result);
+    }
+
+    @Test
+    void testCreateBankAccountWithBalance() {
+        BankAccount account1 = new BankAccount("Kumbajan Aubergino");
+        Integer result = account1.showBalance();
+        assertEquals(0, result);
     }
 
 }
