adc51278f0d609ff7df935f4d50bd8cd9a90033c
Natalia_Sherchenkova <tataterer@gmail.com>
2021-08-24 16:13:47 +0100

    Test for transaction date added (not stubbed) - simple pass

--- bank/src/main/java/bankerPack/Transaction.java
+++ bank/src/main/java/bankerPack/Transaction.java
@@ -2,12 +2,16 @@ package bankerPack;
 
 public class Transaction {
     private Integer amount;
+    private String date;
 
     public Transaction(Integer amount) {
         this.amount = amount;
+        this.date = "24-08-2021";
     }
     public Integer showAmount() {
         return amount;
     }
-
+    public String showDate() {
+        return date;
+    }
 }
--- bank/src/test/java/bankerPack/TransactionTest.java
+++ bank/src/test/java/bankerPack/TransactionTest.java
@@ -6,9 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TransactionTest {
 
     @Test
-    void testAddDepositOne() {
+    void testCreateTransaction() {
         Transaction deposit1 = new Transaction(100);
         Integer result = deposit1.showAmount();
         assertEquals(100, result);
+    }
+    @Test
+    void testTransactionWithDate() {
+        Transaction deposit2 = new Transaction(200);
+        String result = deposit2.showDate();
+        assertEquals("24-08-2021", result);
     }
 }
